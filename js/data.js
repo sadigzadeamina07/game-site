ae3b863a8f4f2c0bf3ce0d517a816390649bec16
sadigzadeamina07 <sadigzadeamina7@gmail.com>
2025-12-21 22:07:09 +0400

    almost completed the task

---
+++ js/data.js
@@ -0,0 +1,70 @@
+const trendingGames = [
+  {
+    id: 1,
+    title: "Assassin Creed",
+    category: "Action",
+    image: "assets/images/trending-01.jpg",
+    oldPrice: 28,
+    price: 20
+  },
+  {
+    id: 2,
+    title: "Assassin Creed",
+    category: "Action",
+    image: "assets/images/trending-02.jpg",
+    price: 44
+  },
+  {
+    id: 3,
+    title: "Assassin Creed",
+    category: "Action",
+    image: "assets/images/trending-03.jpg",
+    oldPrice: 64,
+    price: 44
+  },
+  {
+    id: 4,
+    title: "Assassin Creed",
+    category: "Action",
+    image: "assets/images/trending-04.jpg",
+    price: 32
+  }
+];
+const mostPlayedGames = [
+  {
+    id: 1,
+    title: "Assassin Creed",
+    category: "Adventure",
+    image: "assets/images/top-game-01.jpg"
+  },
+  {
+    id: 2,
+    title: "Assassin Creed",
+    category: "Adventure",
+    image: "assets/images/top-game-02.jpg"
+  },
+  {
+    id: 3,
+    title: "Assassin Creed",
+    category: "Adventure",
+    image: "assets/images/top-game-03.jpg"
+  },
+  {
+    id: 4,
+    title: "Assassin Creed",
+    category: "Adventure",
+    image: "assets/images/top-game-04.jpg"
+  },
+  {
+    id: 5,
+    title: "Assassin Creed",
+    category: "Adventure",
+    image: "assets/images/top-game-05.jpg"
+  },
+  {
+    id: 6,
+    title: "Assassin Creed",
+    category: "Adventure",
+    image: "assets/images/top-game-06.jpg"
+  }
+];
